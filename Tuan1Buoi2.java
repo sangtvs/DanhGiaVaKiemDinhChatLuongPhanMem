b7136d212bb308042efb89103ee2c3dfe416edcd
sangtvs <sangtvs1@gmail.com>
2024-04-19 08:55:50 +0700

    DanhGiaVaKiemDinhChatLuongPhanMem

---
+++ Tuan1Buoi2.java
@@ -0,0 +1,13 @@
+public class Tuan1Buoi2 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        for (int i = 0; i < number; i++) {
+            if (i % 2 == 0) {
+                System.out.println("So " + i + " la so chan.");
+            } else {
+                System.out.println("So " + i + " la so le.");
+            }
+        }
+    }
+}
